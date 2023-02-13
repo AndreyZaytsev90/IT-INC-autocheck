072ae605779217b1d6b4fb1552e54475981d9097
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-02-13 23:18:31 +0300

    Update file

--- hw-autocheck/src/s2-homeworks/hw01/HW1.tsx
+++ hw-autocheck/src/s2-homeworks/hw01/HW1.tsx
@@ -38,7 +38,7 @@ export const message0: MessageType = {
         name: 'Andrey',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'some textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
 }
